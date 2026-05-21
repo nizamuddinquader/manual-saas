4c6034266f8903741a27179bbc13fcf070c18ddc
nizamuddinquader <nizamuddinquader@gmail.com>
2026-05-22 00:37:19 +0600

    add migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',['admin', 'user'])->default('user');
+            $table->boolean('is_active')->default(false);
+            $table->date('subscription_expires_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
